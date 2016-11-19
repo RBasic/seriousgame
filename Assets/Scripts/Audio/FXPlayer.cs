448c6dd5bc3c3fea95be781e72969d0116514b0e
BRUCELIT-PC\Brucelit <simon.galand@gmail.com>
2016-11-19 17:20:34 +0100

    Test Animations
    Test d'animations et de déclenchement d'évenements FMOD

---
+++ Assets/Scripts/Audio/FXPlayer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FXPlayer : MonoBehaviour {
+
+    [FMODUnity.EventRef]
+
+    public string footSteps = "event:/Footsteps";
+    public float poids;
+	
+    // Use this for initialization
+	void Start () {
+        poids = 0.5f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void FootSteps()
+    {
+        FMOD.Studio.EventInstance fs;
+        FMOD.Studio.ParameterInstance Poids;
+        //Poids.setValue(poids);
+        fs = FMODUnity.RuntimeManager.CreateInstance(footSteps);
+        fs.getParameter("Poids", out Poids);
+        fs.start();
+    }
+}

6bfa027422a53bb070f5a9edc094efde35ed15c5
RBasic <rcham.pro@gmail.com>
2016-11-22 00:03:01 +0100

    saut dynamique + début fond salles
    maintenir touche de saut pour sauter plus haut

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerMove : MonoBehaviour {
+public class PlayerMove : MonoBehaviour
+{
     public float maxspeed = 10f;
     bool facingRight = true;
 
@@ -15,11 +16,13 @@ public class PlayerMove : MonoBehaviour {
 
     Animator anim;
 
-    void Start(){
+    void Start()
+    {
         anim = GetComponent<Animator>();
     }
 
-    void FixedUpdate(){
+    void FixedUpdate()
+    {
 
         grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
         anim.SetBool("ground", grounded);
@@ -28,39 +31,48 @@ public class PlayerMove : MonoBehaviour {
         anim.SetFloat("speed", Mathf.Abs(move));
         GetComponent<Rigidbody2D>().velocity = new Vector2(move * maxspeed, GetComponent<Rigidbody2D>().velocity.y);
 
-        if(move>0 && !facingRight){
+        if (move > 0 && !facingRight)
+        {
             flip();
         }
-        else if (move<0 && facingRight){
+        else if (move < 0 && facingRight)
+        {
             flip();
         }
     }
 
-    void Update(){
+    void Update()
+    {
         if (anim.GetFloat("speed") != 0)
         {
             timer = 0.0f;
         }
         timer += Time.deltaTime;
         //Debug.Log("timer = " + timer);
-        if(timer > idleTime)
+        if (timer > idleTime)
         {
             anim.SetBool("wait", true);
             timer = 0.0f;
         }
-        else{
+        else
+        {
             anim.SetBool("wait", false);
         }
 
         if (grounded && Input.GetButtonDown("Jump"))
         {
             anim.SetBool("ground", false);
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(0,jumpForce));
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+        }
+        if(!grounded && !Input.GetButton("Jump") && GetComponent<Rigidbody2D>().velocity.y>-5)
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, -1), ForceMode2D.Impulse);
         }
         anim.SetFloat("wait", Time.deltaTime);
     }
 
-    void flip(){
+    void flip()
+    {
         facingRight = !facingRight;
         Vector3 theScale = transform.localScale;
         theScale.x *= -1;

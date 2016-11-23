61b88254fe6bcf708d228fa5339fde0ab363673b
RBasic <rcham.pro@gmail.com>
2016-11-23 09:53:31 +0100

    saut perso final + plateformes final + lvl design
    Aller dans la scène "Pieces" pour tester le lvl design

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -3,13 +3,15 @@ using System.Collections;
 
 public class PlayerMove : MonoBehaviour
 {
+    bool collided = false;
     public float maxspeed = 10f;
     bool facingRight = true;
-
+    public float maxfall = -20f;
     bool grounded = false;
     public Transform groundCheck;
     float groundRadius = 0.2f;
     public LayerMask whatIsGround;
+    Collider2D col;
     public float jumpForce = 700f;
     float timer = 0.0f;
     public float idleTime = 10.0f;
@@ -23,7 +25,6 @@ public class PlayerMove : MonoBehaviour
 
     void FixedUpdate()
     {
-
         grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
         anim.SetBool("ground", grounded);
         anim.SetFloat("vSpeed", GetComponent<Rigidbody2D>().velocity.y);
@@ -39,10 +40,7 @@ public class PlayerMove : MonoBehaviour
         {
             flip();
         }
-    }
 
-    void Update()
-    {
         if (anim.GetFloat("speed") != 0)
         {
             timer = 0.0f;
@@ -61,14 +59,24 @@ public class PlayerMove : MonoBehaviour
 
         if (grounded && Input.GetButtonDown("Jump"))
         {
+            if (Input.GetAxis("Vertical") < 0 && collided)
+            {
+                //Debug.Log("je suis la !");
+                StartCoroutine(wait1sec());
+            }
             anim.SetBool("ground", false);
             GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
         }
-        if(!grounded && !Input.GetButton("Jump") && GetComponent<Rigidbody2D>().velocity.y>-5)
+
+        if (!grounded && !Input.GetButton("Jump") && GetComponent<Rigidbody2D>().velocity.y > 0)
         {
             GetComponent<Rigidbody2D>().AddForce(new Vector2(0, -1), ForceMode2D.Impulse);
         }
-        anim.SetFloat("wait", Time.deltaTime);
+
+        if (GetComponent<Rigidbody2D>().velocity.y < maxfall)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, maxfall);
+        }
     }
 
     void flip()
@@ -77,5 +85,31 @@ public class PlayerMove : MonoBehaviour
         Vector3 theScale = transform.localScale;
         theScale.x *= -1;
         transform.localScale = theScale;
+    }
+
+    void OnCollisionEnter2D(Collision2D coll){
+        if (coll.collider.gameObject.layer == LayerMask.NameToLayer("thin"))
+        {
+            collided = true;
+            col = coll.collider;
+            //Debug.Log("collided = " + collided);
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D coll)
+    {
+        if (coll.collider.gameObject.layer == LayerMask.NameToLayer("thin"))
+        {
+            collided = false;
+            //Debug.Log("collided = " + collided);
+        }
+    }
+
+    IEnumerator wait1sec()
+    {
+        col.enabled = false;
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(0, -jumpForce), ForceMode2D.Impulse);
+        yield return new WaitForSeconds(0.3f);
+        col.enabled = true;
     }
 }

83aae84963e7e275b4b4e783703fdd5d39e2f7a9
MSI\Diane <diane.primault@gmail.com>
2016-11-28 17:37:45 +0100

    Sound + Ajout scene city au corridor

---
+++ Assets/Scripts/Room/DoorCity.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class DoorCity : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene("city");
+        }
+    }
+}

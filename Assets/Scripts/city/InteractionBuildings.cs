198c982c046a2e0da3376a653aab172fa5da05cb
MSI\Diane <diane.primault@gmail.com>
2016-12-11 00:21:20 +0100

    achats

---
+++ Assets/Scripts/city/InteractionBuildings.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InteractionBuildings : MonoBehaviour {
+
+    [SerializeField]
+    Building currentBuilding;
+
+    void Start()
+    {
+        currentBuilding.setIsSelected(true);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("left"))
+            changeCurrent(currentBuilding.getLeft());
+
+        if (Input.GetKeyDown("right"))
+            changeCurrent(currentBuilding.getRight());
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            currentBuilding.setIsBuy(true);
+        }
+    }
+
+    void changeCurrent(Building newCurrent)
+    {
+        currentBuilding.setIsSelected(false);
+        currentBuilding = newCurrent;
+        currentBuilding.setIsSelected(true);
+
+    }
+}

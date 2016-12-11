85f9ec6f57b789e6e0297b8635ef64de61303a9a
MSI\Diane <diane.primault@gmail.com>
2016-12-11 10:16:42 +0100

    City buy

--- Assets/Scripts/city/InteractionBuildings.cs
+++ Assets/Scripts/city/InteractionBuildings.cs
@@ -1,35 +1,76 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class InteractionBuildings : MonoBehaviour {
 
     [SerializeField]
     Building currentBuilding;
+    [SerializeField]
+    Text textDisplay;
+    bool buttonDown = true;
 
     void Start()
     {
         currentBuilding.setIsSelected(true);
+        currentBuilding.changeOpacitySelected();
+        changeText();
     }
 
     void Update()
     {
-        if (Input.GetKeyDown("left"))
-            changeCurrent(currentBuilding.getLeft());
-
-        if (Input.GetKeyDown("right"))
-            changeCurrent(currentBuilding.getRight());
-
-        if (Input.GetKeyDown(KeyCode.Space))
+        /*
+        foreach (KeyCode vKey in System.Enum.GetValues(typeof(KeyCode)))
         {
-            currentBuilding.setIsBuy(true);
+            if (Input.GetKey(vKey))
+            {
+                Debug.Log(vKey);
+            }
         }
+        */
+        float move = Input.GetAxis("DpadHorizontal");
+        if (!buttonDown && (move == -1 || Input.GetKeyDown("left") || Input.GetKeyDown(KeyCode.LeftArrow)))
+        {
+            changeCurrent(currentBuilding.getLeft());
+            buttonDown = true;
+        }
+
+        else if (!buttonDown && (move == 1 || Input.GetKeyDown("right") || Input.GetKeyDown(KeyCode.RightArrow)))
+        {
+            changeCurrent(currentBuilding.getRight());
+            buttonDown = true;
+        }
+
+        else if (buttonDown && move ==0)
+        {
+            buttonDown = false;
+        }
+
+        else if (Input.GetKeyDown("joystick button 0") || Input.GetKeyDown(KeyCode.Space))
+        {
+            if (!currentBuilding.getIsBuy())
+            {
+                currentBuilding.setIsBuy(true);
+                textDisplay.text = currentBuilding.getTextJustBuying();
+            }
+        }
+
     }
 
     void changeCurrent(Building newCurrent)
     {
+       
         currentBuilding.setIsSelected(false);
+        currentBuilding.changeOpacityNoSeleted();
         currentBuilding = newCurrent;
         currentBuilding.setIsSelected(true);
+        currentBuilding.changeOpacitySelected();
+        changeText();
 
+    }
+
+    void changeText()
+    {
+        textDisplay.text = currentBuilding.getText();
     }
 }

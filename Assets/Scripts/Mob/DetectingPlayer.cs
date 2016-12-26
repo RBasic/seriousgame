5e9ce16eb0e4a23120ad6e332a11075f0229baae
BRUCELIT-PC\Brucelit <simon.galand@gmail.com>
2016-12-26 23:19:19 +0100

    Tuer -> Propager le bien

--- Assets/Scripts/Mob/DetectingPlayer.cs
+++ Assets/Scripts/Mob/DetectingPlayer.cs
@@ -2,8 +2,9 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class DetectingPlayer : MonoBehaviour {
-
+public class DetectingPlayer : MonoBehaviour
+{
+    public bool once = false;
     GameObject enemy;
     TextMesh textShowed;
     List<string> dialogues = new List<string>();
@@ -33,42 +34,51 @@ public class DetectingPlayer : MonoBehaviour {
     {
         if(other.gameObject.tag == "Player")
         {
-            dialogues.Clear();
-            Player p = GameManager.instance.getPlayer();
-            if (CSVReader.instance.getPriority(p.getEthnieString(), "boss", p.getGender()) == 1)
+            if (!once && enemy.transform.FindChild("body").gameObject.activeInHierarchy == true)
             {
-                //Debug.Log(CSVReader.instance.getDialog(p.getEthnieString(), "boss", p.getGender()));
-                dialogues.Add(CSVReader.instance.getDialog(p.getEthnieString(), "boss", p.getGender()));
-            }
-            if (CSVReader.instance.getPriority(p.getSexualityString(), "boss", p.getGender()) == 1)
-            {
-               // Debug.Log(CSVReader.instance.getDialog(p.getSexualityString(), "boss", p.getGender()));
-                dialogues.Add(CSVReader.instance.getDialog(p.getSexualityString(), "boss", p.getGender()));
-            }
-            if (CSVReader.instance.getPriority(p.getHandicapString(), "boss", p.getGender()) == 1)
-            {
-                //Debug.Log(CSVReader.instance.getDialog(p.getHandicapString(), "boss", p.getGender()));
-                dialogues.Add(CSVReader.instance.getDialog(p.getHandicapString(), "boss", p.getGender()));
-            }
-            if (CSVReader.instance.getPriority(p.getBodyString(), "boss", p.getGender()) == 1)
-            {
-                //Debug.Log(CSVReader.instance.getDialog(p.getBodyString(), "boss", p.getGender()));
-                dialogues.Add(CSVReader.instance.getDialog(p.getBodyString(), "boss", p.getGender()));
-            }
-            if(dialogues.Count != 0)
-            {
-                enemy.transform.FindChild("body").gameObject.SetActive(false);
-                enemy.transform.FindChild("bodyT").gameObject.SetActive(true);
-                enemy.GetComponent<EnemyPathing>().setChasingPlayer(true);
-                enemy.GetComponent<EnemyPathing>().setPathing(false);
+                dialogues.Clear();
+                Player p = GameManager.instance.getPlayer();
+                if (CSVReader.instance.getPriority(p.getEthnieString(), "boss", p.getGender()) == 1)
+                {
+                    //Debug.Log(CSVReader.instance.getDialog(p.getEthnieString(), "boss", p.getGender()));
+                    dialogues.Add(CSVReader.instance.getDialog(p.getEthnieString(), "boss", p.getGender()));
+                }
+                if (CSVReader.instance.getPriority(p.getSexualityString(), "boss", p.getGender()) == 1)
+                {
+                    // Debug.Log(CSVReader.instance.getDialog(p.getSexualityString(), "boss", p.getGender()));
+                    dialogues.Add(CSVReader.instance.getDialog(p.getSexualityString(), "boss", p.getGender()));
+                }
+                if (CSVReader.instance.getPriority(p.getHandicapString(), "boss", p.getGender()) == 1)
+                {
+                    //Debug.Log(CSVReader.instance.getDialog(p.getHandicapString(), "boss", p.getGender()));
+                    dialogues.Add(CSVReader.instance.getDialog(p.getHandicapString(), "boss", p.getGender()));
+                }
+                if (CSVReader.instance.getPriority(p.getBodyString(), "boss", p.getGender()) == 1)
+                {
+                    //Debug.Log(CSVReader.instance.getDialog(p.getBodyString(), "boss", p.getGender()));
+                    dialogues.Add(CSVReader.instance.getDialog(p.getBodyString(), "boss", p.getGender()));
+                }
+                if (dialogues.Count != 0)
+                {
+                    SwapAnimators();
+                }
+                else
+                {
+                    dialogues.Add(CSVReader.instance.getDialog(p.getEthnieString(), "boss", p.getGender()));
+                }
+                int rand = Random.Range(0, dialogues.Count);
+                //Debug.Log("je dis = " + dialogues[rand]);
+                textShowed.text = dialogues[rand];
+                once = true;
             }
             else
             {
-                dialogues.Add(CSVReader.instance.getDialog(p.getEthnieString(), "boss", p.getGender()));
+                int rand = Random.Range(0, dialogues.Count);
+                dialogues.Add(CSVReader.instance.getDialog("Blanc", "boss", true));
+                textShowed.text = dialogues[rand];
+                //dialogue positif
             }
-            int rand = Random.Range(0, dialogues.Count);
-            //Debug.Log("je dis = " + dialogues[rand]);
-            textShowed.text = dialogues[rand];
+
         }
     }
 
@@ -79,5 +89,13 @@ public class DetectingPlayer : MonoBehaviour {
             enemy.GetComponent<EnemyPathing>().startSpeedBoostCoroutine();
             //enemy.GetComponent<EnemyPathing>().setChasingPlayer(false);
         }
+    }
+
+    public void SwapAnimators()
+    {
+        enemy.transform.FindChild("body").gameObject.SetActive(!enemy.transform.FindChild("body").gameObject.activeInHierarchy);
+        enemy.transform.FindChild("bodyT").gameObject.SetActive(!enemy.transform.FindChild("bodyT").gameObject.activeInHierarchy);
+        enemy.GetComponent<EnemyPathing>().setChasingPlayer(enemy.GetComponent<EnemyPathing>().getChasingPlayer());
+        enemy.GetComponent<EnemyPathing>().setPathing(!enemy.GetComponent<EnemyPathing>().getPathing());
     }
 }

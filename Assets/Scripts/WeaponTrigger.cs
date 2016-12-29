4d536ed5b47edad0dd641c50e950ab846c14372f
DESKTOP-VPULTRQ\Wohran <hugo.lafon2@gmail.com>
2016-12-29 16:27:00 +0100

    Ajout sons armes
    Sons pour chaque arme
    Je dois encore enlever le son normal de coup dans le vent si on touche
    un ennemi

--- Assets/Scripts/WeaponTrigger.cs
+++ Assets/Scripts/WeaponTrigger.cs
@@ -5,16 +5,34 @@ public class WeaponTrigger : MonoBehaviour
 {
     public GameObject coin;
     private Animator Anim;
+    private string weaponName;
+
+    [FMODUnity.EventRef]
+
+    public string baseballHit = "event:/Serious Game/FX/BaseballBatSwing";
+    public string swordHit = "event:/Serious Game/FX/SwordHit";
+    public string machetteHit = "event:/Serious Game/FX/MachetteHit";
+    public string axeHit = "event:/Serious Game/FX/AxeHit";
+
+    FMOD.Studio.EventInstance baseballBat;
+    FMOD.Studio.EventInstance sword;
+    FMOD.Studio.EventInstance machette;
+    FMOD.Studio.EventInstance axe;
+
     // Use this for initialization
     void Start()
     {
         Anim = transform.parent.gameObject.transform.parent.gameObject.transform.parent.gameObject.GetComponent<Animator>();
-
+        baseballBat = FMODUnity.RuntimeManager.CreateInstance(baseballHit);
+        sword = FMODUnity.RuntimeManager.CreateInstance(swordHit);
+        machette = FMODUnity.RuntimeManager.CreateInstance(machetteHit);
+        axe = FMODUnity.RuntimeManager.CreateInstance(axeHit);
     }
 
     // Update is called once per frame
     void Update()
     {
+        weaponName = transform.GetComponent<SpriteRenderer>().sprite.name;
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -26,6 +44,16 @@ public class WeaponTrigger : MonoBehaviour
             //Debug.Log("1 : " + other);
             if (Anim.GetCurrentAnimatorStateInfo(0).IsName("Attack"))
             {
+                //sound depending on which weapon is equipped
+                if (weaponName == "arme1" || weaponName == "arme4")
+                    axe.start();
+                else if (weaponName == "arme2")
+                    machette.start();
+                else if (weaponName == "arme3")
+                    sword.start();
+                else if (weaponName == "arme5")
+                    baseballBat.start();
+
                 Debug.Log("2 : " + other);
                 int nb = 10;
                 if (!GameManager.instance.getPlayer().getGender())
